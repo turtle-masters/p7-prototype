824bfdb5ffd9b8c1ea0c77d4be26d12654e5ffb5
Fabian <fabiandemeter22@gmail.com>
2021-11-03 15:34:41 +0100

    Update Microverse Looks

--- Assets/Scripts/ChemData.cs
+++ Assets/Scripts/ChemData.cs
@@ -9,30 +9,36 @@ public class ChemData : MonoBehaviour
     public string Name = "";
     public string Formula = "";
     private GameObject chemText;
-    private TextMesh textMesh;
+    //private TextMesh textMesh;
 
     private void Start() {
         if(chemTextPrefab!=null) { //Add text object and set it as child
             chemText = Instantiate(chemTextPrefab,transform.position,Quaternion.identity);
             chemText.transform.SetParent(this.transform);
             //chemText.transform.localPosition.Set(chemText.transform.localPosition.x,chemText.transform.localPosition.y+textYOffset,chemText.transform.localPosition.z);
-            textMesh = chemText.GetComponent<TextMesh>();
-            textMesh.text=Name;
+            SetName(Name);
         }
     }
 
     private void Update() {
         if(Vector3.Distance(transform.position,MinigameManagerScript.playerGameObject.transform.position)<MinigameManagerScript.chemTextDisableDistance) {
-            textMesh.text=Name;
+            SetName(Name);
             chemText.transform.LookAt(MinigameManagerScript.playerGameObject.transform.position);
         } else {
-            textMesh.text = "";
+            SetName("");
         }
     }
 
     public void SetName(string _Name) {
         Name = _Name;
-        if(chemText.GetComponent<TextMesh>())
-            chemText.GetComponent<TextMesh>().text=Name;
+        if(chemText.GetComponent<TextMesh>()) {
+            if(Name=="NAD+") {
+                chemText.GetComponent<TextMesh>().text="Enzyme";
+            } else if(Name=="NADH") {
+                chemText.GetComponent<TextMesh>().text="Reduced\nEnzyme";
+            } else {
+                chemText.GetComponent<TextMesh>().text=Name;
+            }
+        }
     }
 }
