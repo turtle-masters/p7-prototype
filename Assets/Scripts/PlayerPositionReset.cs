ae88fba7dbee8e0aff9167a4f5125cce9423b8ca
kuff <16867891+kuff@users.noreply.github.com>
2021-11-10 11:06:08 +0100

    Reset player rotation as well as position

--- Assets/Scripts/PlayerPositionReset.cs
+++ Assets/Scripts/PlayerPositionReset.cs
@@ -15,13 +15,16 @@ public class PlayerPositionReset : MonoBehaviour
 
     private void OnActiveSceneChanged(Scene oldScene, Scene newScene)
     {
-        Vector3 spawnPointVector = GetSpawnPointVector();
+        (Vector3, Quaternion) spawnPoint = GetSpawnPoint();
+        Vector3 spawnPointVector = spawnPoint.Item1;
+
         this.playerObject.transform.position = new Vector3(spawnPointVector.x, this.playerObject.transform.position.y, spawnPointVector.z);
+        this.playerObject.transform.rotation = spawnPoint.Item2;
     }
 
-    private Vector3 GetSpawnPointVector()
+    private (Vector3, Quaternion) GetSpawnPoint()
     {
         GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
-        return spawnPoints[0].transform.position;
+        return (spawnPoints[0].transform.position, spawnPoints[0].transform.rotation);
     }
 }
